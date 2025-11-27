0a75c0b289439d07adfafc511bfd91cf859371b2
Bediz Oymak <bedizoymak@hotmail.com>
2025-11-27 08:14:55 +0300

    size 80

--- src/components/WhatsAppButton.tsx
+++ src/components/WhatsAppButton.tsx
@@ -12,8 +12,8 @@ const WhatsAppButton = () => {
     >
 
       <svg
-        width="96"
-        height="96"
+        width="80"
+        height="80"
         viewBox="0 0 174 174"
         xmlns="http://www.w3.org/2000/svg"
       >
