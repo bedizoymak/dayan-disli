573412e3ebc02ff415d665a6ea7d7097e780d770
Bediz Oymak <bedizoymak@hotmail.com>
2025-12-09 16:03:25 +0300

    quotation page v10

--- src/features/quotation/components/EmailPreviewModal.tsx
+++ src/features/quotation/components/EmailPreviewModal.tsx
@@ -81,8 +81,8 @@ export function EmailPreviewModal({
 
           {pdfPreviewUrl && (
             <div className="border border-slate-600 rounded-lg overflow-hidden">
-              <iframe 
-                src={pdfPreviewUrl} 
+              <iframe
+            src={`${pdfPreviewUrl}#filename=${encodeURIComponent(teklifNo + ".pdf")}`} 
                 className="w-full h-[300px] bg-white"
                 title="PDF Preview"
               />
